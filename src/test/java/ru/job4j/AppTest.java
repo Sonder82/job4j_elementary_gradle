0b3da7661d78abd092884ea15e1e1321d77fe2b7
Aleksey Novoselov <Himrs82@gmail.com>
2025-01-15 17:19:53 +0500

    Создание проекта на Gradle [#505349]. Исправления по checkstyle

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -6,7 +6,6 @@ package ru.job4j;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 class AppTest {
     @Test
     public void whenActionThenResult() {
